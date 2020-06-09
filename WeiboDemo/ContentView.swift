8d52cee6c774283081dcd7b57528bd7540bfc54f
xiongwei <your811225@hotmail.com>
2020-06-09 11:36:48 +0800

    Initial Commit

---
+++ WeiboDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeiboDemo
+//
+//  Created by 熊伟 on 2020/6/9.
+//  Copyright © 2020 Ultra-Union. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
